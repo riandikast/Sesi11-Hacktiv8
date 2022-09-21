346fceed27f847c27113b37f1ac5359518583e9d
Riandika <riandikasp@gmail.com>
2022-09-22 01:17:52 +0700

    CRUD - Redux

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import { Provider } from 'react-redux'
 import reportWebVitals from './reportWebVitals';
 import store from './store'
-
+// 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 
